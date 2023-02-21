eadaab5b69383a8afdfdec4549567ca5f1d23e89
Aamir Shaikh <aamir0744@gmail.com>
2023-02-21 14:23:26 +0000

    done with displays, working on add task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    debugger
+    @task = Task.new(params[:task])
   end
 end
