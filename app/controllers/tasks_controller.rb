ce71bef32def399e6b891e9998f3d4968238c36d
Aamir Shaikh <aamir0744@gmail.com>
2023-02-21 17:28:04 +0000

    done with delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @tasks = Task.all
   end
@@ -12,7 +13,29 @@ class TasksController < ApplicationController
   end
 
   def create
-    debugger
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
